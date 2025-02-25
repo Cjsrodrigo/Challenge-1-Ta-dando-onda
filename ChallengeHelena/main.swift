1509b754470100bc462910e208a9a0c36be78757
Rodrigo Cont <rodrigocontjoviano@hotmail.com>
2025-02-25 10:47:20 -0300

    Initial Commit

---
+++ ChallengeHelena/main.swift
@@ -0,0 +1,22 @@
+//
+//  main.swift
+//  ChallengeHelena
+//
+//  Created by Aluno 41 on 24/02/25.
+//
+
+import Foundation
+
+print("Essa é uma história sobre surf, família e superação. Para começar, escolha o nome da protagonista feminina:")
+let mainName = readLine()
+
+print("\(mainName!)")
+print("Quais os nomes dos pais da \(mainName!)?")
+let dadName = readLine()
+let momName = readLine()
+
+print("\(mainName!), uma menina nascida e criada em Ubatuba, de família simples e sem luxos. Cresceu com os seus pais, \(dadName!) \(momName!) e desde que se entende por gente o seu passatempo favorito sempre foi surfar com seu pai, que era apaixonado/obcecado pelo esporte e passou a paixão para a filha.")
+
+
+
+
