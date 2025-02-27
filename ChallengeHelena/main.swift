2901483f5e0717e30d43580058b08a7cd6cc5e1f
Aluno 19 <aluno19@10-40-50-30.0200.ecps.br.iosda.org>
2025-02-27 12:08:04 -0300

    implementando torneio final

--- ChallengeHelena/main.swift
+++ ChallengeHelena/main.swift
@@ -58,6 +58,50 @@ if choice == "1"{
 
 //torneio e fim
 
+print("O que garantiu a sua tão sonhada classificação para o Makai foi a manobra final, a que ela aprendeu durante esse último ano de treinamento. Era isto. Ela tinha sido selecionada. Ela iria para o Hawaii. Teria 6 meses para treinar até o Makai.")
+
+print(keepGoing)
+let aux2 = readLine()
+
+print("Chega o momento do tão esperado Campeonato (Makai). As memórias de dias inteiros de treino, sua história e desafios enfrentados se manifestam na mente da atleta. Mesmo com todos os anos de preparação, \(mainName!) sente calafrios e o coração batendo, como se fossem suas primeiras aulas de surf.")
+
+print(keepGoing)
+let aux3 = readLine()
+
+print("O clima do Hawaii é algo surpreendente, ondas enormes, ventos arrasadores, \(mainName!) definitivamente não estava na sua zona de conforto, foi aí que ela percebeu que estava muito longe de casa.")
+
+print("")
+print("Escolha o nome de sua maior rival")
+let rivalName = readLine()
+print("")
+print("Lá, ela conhece sua rival de categoria \(rivalName!), atleta dos EUA, residente das águas havaianas, junto à sua equipe completa de assistentes e patrocinadores.")
+print("Nesse momento, \(mainName!) vê todo o suporte que \(rivalName!) recebe e se dá conta de como as suas realidades foram diferentes: enquanto \(mainName!) passou a vida tendo que conciliar seu sonho com inúmeras outras questões, sua rival teve o privilégio de se preparar com os melhores profissionais, usufruir sempre de materiais de excelência, além de poder se dedicar ao esporte sem outras preocupações.")
+
+print(keepGoing)
+let aux4 = readLine()
+
+print("O sol nasce, o grande dia chegou, \(mainName!) se prepara e organiza seus equipamentos para o momento decisivo, como último cuidado antes da prova, a personagem decide reforçar a parafina de sua prancha, a fim de estar melhor preparada e evitar eventuais contratempos. \(rivalName!) vê a atleta se preparando, se dando conta que esqueceu de levar sua própria parafina e por já estarem na fase preparação pré competição, ela simplesmente não pode sair da área do campeonato para comprar mais, seria trapaça.")
+print("Com isto, \(rivalName!) pergunta para \(mainName!) se poderia usar a dela:")
+print("")
+print("Opção 1: \(mainName!) se empatiza com a rival, lembrando das próprias dificuldades que passou para estar lá, dividindo a parafina, mostrando sua humildade e boa aspiração como atleta e competidora.")
+print("Opção 2: \(mainName!) aproveita a oportunidade e se vê em uma situação de vantagem contra \(rivalName!). Não dividindo a parafina, a fim de prejudicar a norte-americana.")
+
+func choose1(options: Int) -> String {
+    var choice = readLine()
+    if(options == 2){
+        while (choice != "1" && choice != "2") {
+            print("Opção escolhida é inválida\n")
+            choice = readLine()
+        }
+    }else{
+        while (choice != "1" && choice != "2" && choice != "3") {
+            print("Opção escolhida é inválida\n")
+            choice = readLine()
+        }
+    }
+    return choice!
+}
+
 func choose(options: Int) -> String {
     var choice = readLine()
     if(options == 2){
