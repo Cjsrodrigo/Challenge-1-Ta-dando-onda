7492ab7df0a263de5e1c8728ae969e37fb759f2e
sofiavvb <sofiavalverdevb@gmail.com>
2025-02-27 12:03:27 -0300

    before tigrinho

--- ChallengeHelena/main.swift
+++ ChallengeHelena/main.swift
@@ -13,26 +13,27 @@ import Foundation
 print("Essa é uma história sobre surf, família e superação. Para começar, escolha o nome da protagonista feminina:")
 let mainName = readLine()
 
-print("\(mainName!)")
-print("Quais os nomes dos pais da \(mainName!)?")
-let dadName = readLine()
+print("Quais os nome da mãe da \(mainName!)?")
 let momName = readLine()
+
+print("Quais os nome do pai da \(mainName!)?")
+let dadName = readLine()
 var keepGoing = "Aperte qualquer tecla para continuar"
 
-print("\(mainName!), uma menina nascida e criada em Ubatuba, de família simples e sem luxos. Cresceu com os seus pais, \(dadName!) e \(momName!), e desde que se entende por gente o seu passatempo favorito sempre foi surfar com seu pai, que era apaixonado/obcecado pelo esporte e passou a paixão para a filha.\n")
+print("\(mainName!), uma menina nascida e criada em Ubatuba, de família simples e sem luxos. Cresceu com os seus pais, \(dadName!) e \(momName!), e desde que se entende por gente o seu passatempo favorito sempre foi surfar com seu pai, que era apaixonado/obcecado pelo esporte e passou a paixão para a filha.")
 
 // Talvez incluir animacao no terminal entre falas?
 
-print("O vínculo dos dois era muito forte. Para eles, o surf sempre foi mais que um esporte, era um estilo de vida, uma filosofia. \(mainName!) cresceu ouvindo as histórias de ondas gigantes surfadas pelo pai, sobre os poderes misteriosos do oceano e a história mais recorrente: o lendário campeonato no Hawaii, o Makai.\n")
+print("O vínculo dos dois era muito forte. Para eles, o surf sempre foi mais que um esporte, era um estilo de vida, uma filosofia. \(mainName!) cresceu ouvindo as histórias de ondas gigantes surfadas pelo pai, sobre os poderes misteriosos do oceano e a história mais recorrente: o lendário campeonato no Hawaii, o Makai.")
 
 print(keepGoing)
-let aux = readLine()
+var aux = readLine()
 
 //certo dia ... morte do pai
 print("Certo final de semana, \(mainName!) acorda no meio da madrugada com os barulhos de seu pai se preparando para mais uma viagem de surf dele.\n")
 
 print("** \(mainName!), opta por: **\n 1: Voltar a dormir \n 2: Falar com o pai e o deseja boas ondas")
-var choice = choose(options: 2)
+var choice = choose(2)
 
 print("\(mainName!) acorda no dia seguinte com barulhos muito altos de choro vindo da sala. Ela resolve ir ver o que está acontecendo e se depara com o seu pior pesadelo tornando-se realidade: o pai sofreu um acidente de carro fatal a caminho da praia.\n")
 
@@ -45,8 +46,23 @@ if choice == "1"{
 
 // investir no surf + tigrinho
 
+print("Agora, \(mainName!) se encontra no funeral do pai e percebe um homem estranho a observando de longe com um olhar de pena e como se quisesse lhe dizer algo. De repente, o homem começa a se aproximar de \(mainName!), ainda olhando fixamente para ela.")
 
+print("** \(mainName!), opta por: **\n 1: Esperar o homem se aproximar e ver se ele tem algo a dizer. \n 2: Evitar contato com o homem, fugindo dele.")
+choice = choose(2)
 
+if choice == "1"{
+    print("O homem misterioso chega perto de Helena e diz:\n- Com licença, eu preciso mostrar uma coisa que seu pai deixou antes de morrer. Pode me acompanhar até o meu carro?")
+    print("** \(mainName!), opta por: **\n 1: Ir com o estranho até o carro e descobrir o que ele deseja mostrar. \n 2: Dizer que não pode e se afastar dele.")
+    let innerChoice = choose(2)
+    if innerChoice == "1"{
+        print("Ao se encontrar com o estranho, a personagem recebe uma caixa, maior que ela mesma e, no seu conteúdo, há uma foto antiga dela com seu pai, dinheiro e uma prancha de surf feita pelo \(dadName!) para seu aniversário de 18 anos.")
+    }else{
+        print("Helena vai correndo para a sua casa, ainda um pouco assustada com o estranho que estava se aproximando. Ao chegar lá, fica um tempo chorando com a morte.")
+        print(keepGoing)
+        aux = readLine()
+    }
+}
 
 
 
@@ -58,7 +74,7 @@ if choice == "1"{
 
 //torneio e fim
 
-func choose(options: Int) -> String {
+func choose(_ options: Int) -> String {
     var choice = readLine()
     if(options == 2){
         while (choice != "1" && choice != "2") {
@@ -73,5 +89,3 @@ func choose(options: Int) -> String {
     }
     return choice!
 }
-
-
