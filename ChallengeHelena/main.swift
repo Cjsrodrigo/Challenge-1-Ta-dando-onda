e817e1492dd5bc6cdfa01bf375b464b0bb5ce9fe
Aluno 19 <aluno19@Henrique-Lacerda-Silveira.local>
2025-02-28 10:47:43 -0300

    torneio e fim henrique

--- ChallengeHelena/main.swift
+++ ChallengeHelena/main.swift
@@ -7,7 +7,6 @@
 
 import Foundation
 
-
 // introducao inicial e captacao das variaveis dos nomes
 
 print("Essa é uma história sobre surf, família e superação. Para começar, escolha o nome da protagonista feminina:")
@@ -58,24 +57,23 @@ if choice == "1"{
 
 //torneio e fim
 
-print("O que garantiu a sua tão sonhada classificação para o Makai foi a manobra final, a que ela aprendeu durante esse último ano de treinamento. Era isto. Ela tinha sido selecionada. Ela iria para o Hawaii. Teria 6 meses para treinar até o Makai.")
+print("O que garantiu a sua tão sonhada classificação para o Makai foi a manobra final, a que ela aprendeu durante esse último ano de treinamento. Era isto. Ela tinha sido selecionada. Ela iria para o Hawaii. Teria 6 meses para treinar até o Makai.\n")
 
 print(keepGoing)
 let aux2 = readLine()
 
-print("Chega o momento do tão esperado Campeonato (Makai). As memórias de dias inteiros de treino, sua história e desafios enfrentados se manifestam na mente da atleta. Mesmo com todos os anos de preparação, \(mainName!) sente calafrios e o coração batendo, como se fossem suas primeiras aulas de surf.")
+print("Chega o momento do tão esperado Campeonato (Makai). As memórias de dias inteiros de treino, sua história e desafios enfrentados se manifestam na mente da atleta. Mesmo com todos os anos de preparação, \(mainName!) sente calafrios e o coração batendo, como se fossem suas primeiras aulas de surf.\n")
 
 print(keepGoing)
 let aux3 = readLine()
 
-print("O clima do Hawaii é algo surpreendente, ondas enormes, ventos arrasadores, \(mainName!) definitivamente não estava na sua zona de conforto, foi aí que ela percebeu que estava muito longe de casa.")
+print("O clima do Hawaii é algo surpreendente, ondas enormes, ventos arrasadores, \(mainName!) definitivamente não estava na sua zona de conforto, foi aí que ela percebeu que estava muito longe de casa.\n")
 
-print("")
-print("Escolha o nome de sua maior rival")
+print("Escolha o nome de sua maior rival\n")
 let rivalName = readLine()
 print("")
 print("Lá, ela conhece sua rival de categoria \(rivalName!), atleta dos EUA, residente das águas havaianas, junto à sua equipe completa de assistentes e patrocinadores.")
-print("Nesse momento, \(mainName!) vê todo o suporte que \(rivalName!) recebe e se dá conta de como as suas realidades foram diferentes: enquanto \(mainName!) passou a vida tendo que conciliar seu sonho com inúmeras outras questões, sua rival teve o privilégio de se preparar com os melhores profissionais, usufruir sempre de materiais de excelência, além de poder se dedicar ao esporte sem outras preocupações.")
+print("Nesse momento, \(mainName!) vê todo o suporte que \(rivalName!) recebe e se dá conta de como as suas realidades foram diferentes: enquanto \(mainName!) passou a vida tendo que conciliar seu sonho com inúmeras outras questões, sua rival teve o privilégio de se preparar com os melhores profissionais, usufruir sempre de materiais de excelência, além de poder se dedicar ao esporte sem outras preocupações.\n")
 
 print(keepGoing)
 let aux4 = readLine()
@@ -86,22 +84,16 @@ print("")
 print("Opção 1: \(mainName!) se empatiza com a rival, lembrando das próprias dificuldades que passou para estar lá, dividindo a parafina, mostrando sua humildade e boa aspiração como atleta e competidora.")
 print("Opção 2: \(mainName!) aproveita a oportunidade e se vê em uma situação de vantagem contra \(rivalName!). Não dividindo a parafina, a fim de prejudicar a norte-americana.")
 
-func choose1(options: Int) -> String {
-    var choice = readLine()
-    if(options == 2){
-        while (choice != "1" && choice != "2") {
-            print("Opção escolhida é inválida\n")
-            choice = readLine()
-        }
-    }else{
-        while (choice != "1" && choice != "2" && choice != "3") {
-            print("Opção escolhida é inválida\n")
-            choice = readLine()
-        }
-    }
-    return choice!
-}
+var choice2 = choose(options: 2)
 
+if choice2 == "1" {
+    print("Helena passa por sua prova desafiadora e vence o campeonato, lembrando da sua árdua jornada e as memórias com seu pai.")
+} else if choice2 == "2" {
+    print("Helena passa dificuldades durante sua prova, a parafina estava vencida, fazendo com que a mesma tome um caldo, caia e se desclassifique do torneio.")
+} else{
+    print("Opção escolhida é inválida\n")
+}
+ 
 func choose(options: Int) -> String {
     var choice = readLine()
     if(options == 2){
